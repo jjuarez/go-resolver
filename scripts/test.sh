5b60264acdefe566492df1f5f22adc8098ebfd93
Javier Juarez <javier.juarez@fon.com>
2018-04-05 12:23:36 +0200

    Improvements in the wrapper script

---
+++ scripts/test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+declare -r DEFAULT_DEBUG_LEVEL=2
+declare -r DEFAULT_TARGET_HOST="sts.amazonaws.com"
+
+TARGET_HOST="${1:-${DEFAULT_TARGET_HOST}}"
+DEBUG_LEVEL="${2:-${DEFAULT_DEBUG_LEVEL}}"
+
+echo "Testing with go DNS support:"
+GODEBUG="netdns=go+${DEBUG_LEVEL}" "${GOPATH}//bin/resolver" -host="${TARGET_HOST}"
+
+echo "Testing with cgo system DNS support:"
+GODEBUG="netdns=cgo+${DEBUG_LEVEL}" "${GOPATH}/bin/resolver" -host="${TARGET_HOST}"
