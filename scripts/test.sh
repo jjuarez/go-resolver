ff082a675e7b2f13b07d99ccecd5f6aa74d323e9
Javier Juarez <javier.juarez@fon.com>
2018-04-05 12:22:56 +0200

    Better handle of the paths

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ TARGET_HOST="${1:-${DEFAULT_TARGET_HOST}}"
 DEBUG_LEVEL="${2:-${DEFAULT_DEBUG_LEVEL}}"
 
 echo "Testing with go DNS support:"
-GODEBUG="netdns=go+${DEBUG_LEVEL}" ../bin/resolver -host="${TARGET_HOST}"
+GODEBUG="netdns=go+${DEBUG_LEVEL}" "${GOPATH}//bin/resolver" -host="${TARGET_HOST}"
 
 echo "Testing with cgo system DNS support:"
-GODEBUG="netdns=cgo+${DEBUG_LEVEL}" ../bin/resolver -host="${TARGET_HOST}"
+GODEBUG="netdns=cgo+${DEBUG_LEVEL}" "${GOPATH}/bin/resolver" -host="${TARGET_HOST}"
